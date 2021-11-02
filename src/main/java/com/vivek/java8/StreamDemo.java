8d00f541c19935f4706cec1975dd89a8fd668d20
vivekthakare <vivekthakarey@gmail.com>
2021-11-02 00:52:24 -0400

    stream demo

--- src/main/java/com/vivek/java8/StreamDemo.java
+++ src/main/java/com/vivek/java8/StreamDemo.java
@@ -4,11 +4,33 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class StreamDemo {
 
     public static void main(String[] args) {
         final List<Transaction> transactions = populateData();
+        reverseSortTransactions(transactions);
+        matchOperations(transactions);
+        reduceOperations(transactions);
+    }
+
+    private static void reduceOperations(List<Transaction> transactions){
+        System.out.println( "Size : " + transactions.size());
+        System.out.println("max value = " + transactions.stream().map(Transaction::getValue).reduce(1, Integer::max));
+        System.out.println("min value = " + transactions.stream().map(Transaction::getValue).min(Comparator.comparingInt(Integer::intValue)));
+        System.out.println("Product value = " + transactions.stream().map(Transaction::getValue).reduce(1, (a,b) -> a*b));
+        System.out.print("Range 1-10 = ");IntStream.range(1, 10).forEach(System.out::print);
+        System.out.println("RangeClosed 1-10 =");IntStream.rangeClosed(1, 10).forEach(System.out::print);
+    }
+
+    private static void matchOperations(List<Transaction> transactions){
+        System.out.println("allMatch = " + transactions.stream().allMatch(transaction -> transaction.value > 50));
+        System.out.println("anyMatch = " + transactions.stream().anyMatch(transaction -> transaction.value > 50));
+        System.out.println("noneMatch = " + transactions.stream().noneMatch(transaction -> transaction.value > 50));
+    }
+
+    private static void reverseSortTransactions(List<Transaction> transactions) {
         System.out.println("Transactions Before sorting = " + transactions);
         List<Long> transactionIds = transactions.stream().filter(transaction -> Category.GROCERY == transaction.getCategory())
                 .sorted(Comparator.comparingInt(Transaction::getValue).reversed())
